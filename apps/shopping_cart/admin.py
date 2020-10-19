35ece88b18dfab48d4d325a806aba24ad5897c56
yeti <marco2391@hotmail.com>
2020-10-19 19:08:25 -0300

    todo ok el shopping_cart para cursos. Siguiente paso agregar una views

--- apps/shopping_cart/admin.py
+++ apps/shopping_cart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrderItem, Order, Payment
 
 # Register your models here.
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Payment)
--- apps/shopping_cart/models.py
+++ apps/shopping_cart/models.py
@@ -1,16 +1,31 @@
 from django.conf import settings
 from django.db import models
-
+from apps.content.models import Course
 # Create your models here.
 
-#class OrderItem(models.Model):
+class OrderItem(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.course.name
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
     is_ordered = models.BooleanField(default=False)
-    #items
+    items = models.ManyToManyField(OrderItem)
     ref_code = models.CharField(max_length=50)
 
     def __str__(self):
         return self.user.username
+
+#para seguimiento de las compras realizadas:
+class Payment(models.Model):
+    #revisar si en produccion combiene usar on_delete CASCADE ya que de borrarse
+    #un payment se estaría borrando la orden tambien!!!!!
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    total_amount = models.FloatField()
+    date_paid = models.DateTimeField(auto_now_add=True)
+    stripe_charge_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.stripe_charge_id
